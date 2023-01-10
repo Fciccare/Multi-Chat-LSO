f126b0f46ccc510020266f9a288021dfa6ecce96
StDefenza <st.defenza@studenti.unina.it>
2023-01-10 20:10:16 +0100

    Inizio Sviluppo DB Functions

--- Server-C/database/database.c
+++ Server-C/database/database.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include "sqlite3.h"
+#include <string.h>
+#include <stdbool.h>
 
-int main(){
+int createTable();
+bool isExistingUser();
+
+    int main()
+{
     sqlite3* db;
     sqlite3_open("database.db", &db);
     
     return 0;
+} 
+
+/* SQL query for creating tables */
+int createTable(){
+    char query[100] = "CREATE TABLE IF NOT EXIST Users("
+                      "username VARCHAR(20) PRIMARY KEY, "
+                      "password VARCHAR(20) NOT NULL,"
+                      "image BLOB ;)"; /*da vedere*/
+
+        return 0;
+}
+
+bool isExistingUser(){
+    /*TODO*/
 }
