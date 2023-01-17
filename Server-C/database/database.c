1d0131342682d6ff05dc0e4ca440b920664c8439
StDefenza <st.defenza@studenti.unina.it>
2023-01-17 16:13:05 +0100

    Creata funzione di creazione database

--- Server-C/database/database.c
+++ Server-C/database/database.c
@@ -2,21 +2,35 @@
 #include "library/sqlite3.h"
 #include "database.h"
 #include <string.h>
-#include <stdbool.h>
+
+void errorHandler(char[]);
+
+sqlite3* db;
 
 int main(){
-    sqlite3* db;
     sqlite3_open("database.db", &db);
-    
+    createTable();
+    sqlite3_close(db);
     return 0;
 } 
 
 /* SQL query for creating tables */
 void createTable(){
-    char query[] =    "CREATE TABLE IF NOT EXIST Users("
-                      "username VARCHAR(20) PRIMARY KEY, "
-                      "password VARCHAR(20) NOT NULL,"
-                      "image BLOB ;)"; /*da vedere*/
+    char query[] =
+        //"DROP TABLE IF EXISTS Users;"
+        "CREATE TABLE IF NOT EXISTS Users("
+        "username VARCHAR(25) PRIMARY KEY, "
+        "password VARCHAR(20) NOT NULL, "
+        "image BLOB);";
+
+    if(sqlite3_exec(db, query, NULL, NULL, NULL) == SQLITE_OK){
+        printf("Table created or alreary created\n");
+    } else errorHandler("Error on table creation");
+}
+
+void errorHandler(char text[]){
+    perror(text);
+    sqlite3_close(db);
 }
 
 bool isExistingUser(){
--- Server-C/database/database.h
+++ Server-C/database/database.h
@@ -1,6 +1,8 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <stdbool.h>
+
 void createTable();
 bool isExistingUser();
 
