c180beda0b8480634c6382b40777d3156da71b9f
valenty29 <valentin.annunziata@studenti.unina.it>
2023-01-18 19:51:29 +0100

    fix build file

--- Server-C/s_build_and_run.sh
+++ Server-C/s_build_and_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -o server -pthread server.c objects/user.c objects/client.c objects/room.c handler/socket_handler.c database/database.c database/library/sqlite3.c && ./server
+gcc  server.c objects/user.c objects/client.c objects/room.c handler/socket_handler.c database/database.c database/library/sqlite3.c -o server -ldl -pthread && ./server
