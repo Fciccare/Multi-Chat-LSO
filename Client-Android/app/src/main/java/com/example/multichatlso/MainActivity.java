6e2b89cb1391885edd88f57de2c52be23e84dc11
Fciccare <76161641+Fciccare@users.noreply.github.com>
2023-02-01 17:43:25 +0100

    Test git pt2

--- Client-Android/app/src/main/java/com/example/multichatlso/MainActivity.java
+++ Client-Android/app/src/main/java/com/example/multichatlso/MainActivity.java
@@ -12,4 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //Voglio fare un test per git =)
+    //Voglio farne anche un altro -F
 }
